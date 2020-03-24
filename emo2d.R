f570135a8629c8129c3a1f5ed936a00f5cb41bba
Diana <dianaritere@protonmail.com>
2020-03-24 23:07:37 +0200

    finished ggplot without lemmas

--- emo2d.R
+++ emo2d.R
@@ -13,6 +13,9 @@ getwd()
 
 emo <- read.csv("emo_2d_coded.csv", header = T, na.strings=c("", "NA"))
 emo_words <- read.csv("emo_words.csv")
+names(emo_words)[names(emo_words)=="q1"] <- "word"
+names(emo_words)[names(emo_words)=="av1"] <- "valence"
+names(emo_words)[names(emo_words)=="ai1"] <- "activity"
 
 q1_map <- ggplot(emo, aes(x=av1, y=ai1, col = q1, label = q1)) +
   geom_point() +
@@ -24,23 +27,32 @@ q1_map <- ggplot(emo, aes(x=av1, y=ai1, col = q1, label = q1)) +
                      labels=c("-4","-3","-2","-1","0","1","2","3","4")) +
   scale_y_continuous(breaks=-4:4,
                      labels = c("-4","-3","-2","-1","0","1","2","3","4")) +
-  geom_hline(yintercept = 0) + geom_vline(xintercept = 0) 
+  geom_hline(yintercept = 0) + geom_vline(xintercept = 0)
 
 q1_map
 
 head(emo_words)
-emo_words$q1 <- tolower(emo_words$q1)
+# darbības, lai radītu pilno sarakstu ar emociju vārdiem
+emo_words$word <- tolower(emo_words$word) #eliminate uppercase
+no_punct_full <- as.data.frame(gsub("[[:punct:]]", "", emo_words$word))
+no_punct_valence <- data.frame(no_punct_full, emo_words$valence) 
+no_punct_dim <- data.frame(no_punct_valence, emo_words$activity)
+names(no_punct_dim)[names(no_punct_dim)=="gsub.....punct..........emo_words.word."] <- "word"
+names(no_punct_dim)[names(no_punct_dim)=="emo_words.valence"] <- "valence"
+names(no_punct_dim)[names(no_punct_dim)=="emo_words.activity"] <- "activity"
 
-wordfreq <- count(emo_words, "q1")
-no_punctuation <- as.data.frame(gsub("[[:punct:]]", "", wordfreq$q1))
+
+wordfreq <- count(emo_words, "word") #vārdu biežumu kolonna
+no_punctuation <- as.data.frame(gsub("[[:punct:]]", "", wordfreq$word)) #biežumu kolonna bez special symbols
+
+
+#darbības ar mazajiem datiem
 tidied <- data.frame(no_punctuation, wordfreq$freq)
 names(tidied)[names(tidied)=="gsub.....punct..........wordfreq.q1."] <- "word"
 names(tidied)[names(tidied)=="wordfreq.freq"] <- "frequency"
 head(tidied)
-# desc_freq <- sort(tidied$frequency, decreasing=TRUE)
-# desc_freq
 
-small_units <- subset(tidied, (nchar(as.character(word)) <= 20) & frequency > 3)
+small_units <- subset(tidied, (nchar(as.character(word)) <= 20) & frequency > 4)
 
 devtools::install_github('cttobin/ggthemr')
 library(ggthemr)
@@ -52,9 +64,4 @@ ggplot(data = small_units, mapping = aes(x = reorder(word, frequency), frequency
   coord_flip() + 
   scale_x_discrete("Visbiežāk izmantotie emociju vārdi") +
   scale_y_continuous("Biežums")
-
-
-
-# desc_freq <- sort(tidied$frequency, decreasing=TRUE)
-# desc_freq
 
