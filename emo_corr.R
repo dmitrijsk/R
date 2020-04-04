869498db1dad49c5d31dbf9b39085875a1f2592a
Dmitrijs Kašs <dmitrijs.kass@creamfinance.com>
2020-04-05 01:21:36 +0300

    Added comments and corrected / adjusted few parts of the code to make it more clear. Other comments / suggestions in the e-mail.

--- emo_corr.R
+++ emo_corr.R
@@ -1,85 +1,114 @@
-rm(list = ls())
-install.packages("dplyr")
-install.packages("tibble")
-install.packages("ggplot2")
+# rm(list = ls()) # Dmitrijs: This is a bad and dangerous style, we discussed it during the lectures. Use "Restart R" instead.
+
+# Dmitrijs: Comment out the `install.packages()` because is needs to be executed only once.
+# install.packages("dplyr")
+# install.packages("tibble")
+# install.packages("ggplot2")
+
 library(ggplot2)
 library(dplyr)
 library(tibble)
 
-setwd("/home/yes_no/Downloads/bakalaurs")
-getwd()
+# Please read this: https://community.rstudio.com/t/project-oriented-workflow-setwd-rm-list-ls-and-computer-fires/3549
+# and replace `setwd()`.
+# setwd("/home/yes_no/Downloads/bakalaurs")
+# getwd()
 
-emo_start <- read.csv("emo_2d_coded.csv", header = T)
+emo_start <- read.csv("emo_2d_coded.csv", header = TRUE, encoding = "UTF-8")
 
 emo_start <- emo_start %>% 
-  dplyr::rename(
-    ai22 = ai23,
-    ai23 = ai23.1
-  )
+  rename(ai22 = ai23, # `dplyr::` is not needed becusebecause you use `library()` above.
+         ai23 = ai23.1)
 
 colnames(emo_start)
 
 
-#atlasa vajadzīgās kolonnas Zviedru kodola afekta skalas aprēķiniem
+# Atlasa vajadzīgās kolonnas Zviedru kodola afekta skalas aprēķiniem.
 emo_full <- emo_start %>% 
-  dplyr::select(starts_with("id"),starts_with("vecums"), starts_with("dzimums"),starts_with("scas"),starts_with("av"), starts_with("ai"))
-emo_full$observation <- 1:nrow(emo_full)   
+  select(starts_with("id"), 
+         starts_with("vecums"), 
+         starts_with("dzimums"),
+         starts_with("scas"),
+         starts_with("av"), 
+         starts_with("ai")) %>% 
+  mutate(observation = row_number())
 
 nrow(emo_full)
 nrow(na.omit(emo_full))
 
-#izveido apakšskalas
+# Izveido apakšskalas.
 scas_val <- emo_full %>% 
-  dplyr::select(scas_apmierinats, scas_priecigs, scas_laimigs)
+  select(scas_apmierinats, scas_priecigs, scas_laimigs)
 
 scas_activ <- emo_full %>% 
-  dplyr::select(scas_modrs, scas_energisks, scas_aktivs)
+  select(scas_modrs, scas_energisks, scas_aktivs)
 
 scas_paud <- emo_full %>% 
-  dplyr::select(scas_ieinteresets, scas_iesaistits, scas_optimisms)
+  select(scas_ieinteresets, scas_iesaistits, scas_optimisms)
   
 scas_uapd <- emo_full %>% 
-  dplyr::select(scas_rams, scas_mierigs, scas_atvieglots)
+  select(scas_rams, scas_mierigs, scas_atvieglots)
 
 
-#izrēķina vidējos apakšskalas rādītājus katram respondentam
-scas_val$mean_val = rowMeans(scas_val)
-scas_activ$mean_activ = rowMeans(scas_activ)
+# Izrēķina vidējos apakšskalas rādītājus katram respondentam.
+scas_val$mean_val <- rowMeans(scas_val)
+scas_activ$mean_activ = rowMeans(scas_activ) # Replace `=` with `<-`.
 scas_paud$mean_paud = rowMeans(scas_paud)
 scas_uapd$mean_uapd = rowMeans(scas_uapd)
 
 
-emo_full[, 'scas_val_mean'] <- scas_val$mean_val
-emo_full[, 'scas_activ_mean'] <- scas_activ$mean_activ
+emo_full[, "scas_val_mean"] <- scas_val$mean_val
+emo_full[, 'scas_activ_mean'] <- scas_activ$mean_activ # Use double quotation marks.
 emo_full[, 'scas_paud_mean'] <- scas_paud$mean_paud
 emo_full[, 'scas_uapd_mean'] <- scas_uapd$mean_uapd
 
-#sagatavo tabulu scas plot
+# Sagatavo tabulu scas plot.
 scas_full <- cbind(scas_val$mean_val, scas_activ$mean_activ, scas_paud$mean_paud, scas_uapd$mean_uapd)
 
-#valences aktivitātes ggplot
-val_activ <- ggplot(emo_full, aes(x=scas_val_mean, y=scas_activ_mean, col = observation)) +
-  geom_point(size=3, alpha=0.7, position=position_jitter(w=0.1, h=0)) +
-  geom_text(aes(label=observation), check_overlap = FALSE) +
-  geom_hline(yintercept = 4) + geom_vline(xintercept = 4)+
+# Valences aktivitātes ggplot
+library(ggrepel)
+
+data1 <- emo_full %>%
+  select(observation, scas_val_mean, scas_activ_mean) %>% 
+  na.omit()
+
+val_activ <- data1 %>% # `data1` and `data2` are very bad names - please change.
+  # Also change the names of variables, they are hard to understand.
+  ggplot(aes(x = scas_val_mean, y = scas_activ_mean, label = observation)) +
+  # I see no reason for using colour here.
+  geom_jitter(size = 3, alpha = 0.7, width = 0.05, height = 0.05) + # Why only in 1 direction?
+  geom_text_repel(size = 3) +
+  geom_hline(yintercept = 4) + 
+  geom_vline(xintercept = 4) +
+  coord_cartesian(xlim = c(0,8), ylim = c(0,8)) + # Controls the limits of x and y.
   theme_bw()
 
 val_activ
 
-#patīkamas/nepatīkamas aktivitātes/deaktivitātes ggplot
-paud_uapd <- ggplot(emo_full, aes(x=scas_paud_mean, y=scas_uapd_mean)) +
-  geom_point(size=3, alpha=0.7, position=position_jitter(w=0.1, h=0)) +
-  geom_text(aes(label=observation), check_overlap = T) + 
-  geom_hline(yintercept = 4) + geom_vline(xintercept = 4)+
-  theme_bw() 
+
+
+# Patīkamas/nepatīkamas aktivitātes/deaktivitātes ggplot.
+
+data2 <- emo_full %>%
+  select(observation, scas_paud_mean, scas_uapd_mean) %>%
+  na.omit()
+
+paud_uapd <- data2 %>% 
+  ggplot(aes(x = scas_paud_mean, y = scas_uapd_mean, label = observation)) +
+  geom_jitter(size = 3, alpha = 0.7, width = 0.05, height = 0.05) + # Why only in 1 direction?
+  geom_text_repel(size = 3) +
+  geom_hline(yintercept = 4) + 
+  geom_vline(xintercept = 4) +
+  coord_cartesian(xlim = c(0,8), ylim = c(0,8)) + # Controls the limits of x and y.
+  theme_bw()
 
 paud_uapd
 
+
+
+
 stimulus_values <- emo_start %>% 
-  dplyr::select(starts_with("av"), starts_with("ai"))
+  select(starts_with("av"), starts_with("ai"))
 
 stimulus_values <- emo_start %>% 
-  dplyr::select(starts_with("av"), starts_with("ai"))
-
-
-
+  select(starts_with("av"), starts_with("ai"))
